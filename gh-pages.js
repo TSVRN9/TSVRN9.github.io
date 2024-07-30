98e0dfeb32b285f1c660cc54c12b9601fbab2a43
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2024-07-30 12:26:31 -0400

    Create publish workflow to gh-pages

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+import ghpages from 'gh-pages';
+
+const DOMAIN = 'owenwang.dev';
+
+ghpages.publish('build', {
+    nojekyll: true,
+    cname: DOMAIN,
+}, err => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Published successfully' + (DOMAIN ? `to https://${DOMAIN}` : ''));
+    }
+});
+
+
