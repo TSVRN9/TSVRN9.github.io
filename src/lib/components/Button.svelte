6321da47348fa3f9eccd282ded20664a59ed85b7
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2024-07-31 11:46:52 -0400

    Create button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher<{click: MouseEvent}>();
+
+    export let disabled: boolean = false;
+    
+    function handleClick(event: MouseEvent) {
+        if (disabled) return;
+        dispatch("click", event);
+    }
+</script>
+
+<button on:click={handleClick} class="bg-primary-600 text-white py-2 px-4 m-4 text-2xl rounded-md">
+    <slot />
+</button>
