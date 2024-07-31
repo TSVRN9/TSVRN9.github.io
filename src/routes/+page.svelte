58e1495c390c5886696e6fed0e55d644979714bd
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2024-07-31 19:30:52 -0400

    Add content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Owen Wang</title>
+</svelte:head>
+
+<main class="w-4/5 md:w-1/2 lg:w-1/3 mx-auto mt-4 ">
+    <h1 class="text-4xl text-center">Owen Wang</h1>
+    <div class="flex flex-col gap-y-2 mt-4">
+        <h2>Some Projects</h2>
+        <ul class="list-disc ml-8">
+            <li>
+                <a href="https://zyfo.ai" class="text-nowrap">
+                    <span class="bg-gray-800 hover:bg-gray-900 rounded-full text-white py-1 px-4 group">
+                        <span>
+                            Zyfo<span class="bg-gradient-to-tr from-rose-400 via-fuchsia-500 to-indigo-500 inline-block text-transparent bg-clip-text font-bold">.ai</span>
+                        </span>
+                    </span>
+                    - AI Website Maker
+                </a></li>
+            <li><a href="https://github.com/TSVRN9">Github</a></li>
+            <li><a href="https://owenwang.dev/Transcribe">Transcribe</a></li>
+            <li><a href="https://owenwang.dev/SortingVisualizer">Sorting Visualizer</a></li>
+            <li><a href="https://www.athletic.net/athlete/19025995/cross-country/high-school">Athletic.net</a></li>
+        </ul>
+        <h2>Some Skills...</h2>
+        <ul class="list-disc ml-8">
+            <li>Python</li>
+            <li>Java</li>
+            <li>Cross Country
+                <ul class="list-disc ml-8">
+                    <li>5k - 18:29</li>
+                </ul>
+            </li>
+            <li>Web Development!!!
+                <ul class="list-disc ml-8">
+                    <li>Typescript</li>
+                    <li>React</li>
+                    <li>TailwindCSS</li>
+                    <li>Vanilla HTML/JS</li>
+                    <li>Svelte!!!</li>
+                </ul>
+            </li>
+            <li>Piano
+                <ul class="list-disc ml-8">
+                    <li>Hall High School Concert Jazz Band 2023-2024
+                        <ul class="list-disc ml-8">
+                            <li>Downbeat Best High School Jazz Ensemble</li>
+                            <li>Downbeat Best High School Latin Jazz Ensemble</li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li>Machine Learning
+                <ul class="list-disc ml-8">
+                    <li><a href="https://github.com/TSVRN9/BeginnerNeuralNetwork">Handwritten Digits Perceptron</a></li>
+                </ul>
+            </li>
+            <li>Minecraft Plugins
+                <ul class="list-disc ml-8">
+                    <li><a href="https://github.com/TSVRN9/MinecraftManhunt">Minecraft Manhunt</a></li>
+                </ul>
+            </li>
+        </ul>
+        <h2>Contact Me!</h2>
+    </div>
+    <!-- button on:click={() => { alert('Clicked!'); }} class="bg-primary-600 text-white py-2 px-4 m-4 text-2xl rounded-md">Click Me</button> -->
+</main>
+
+<script lang="ts">
+
+</script>
