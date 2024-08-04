dccbf124dcdc47b3a4a80d2c3958237c2a18c995
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2024-08-03 22:15:13 -0400

    Begin work on flare for card

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	let styles = {
-        xPercent: 50,
-        yPercent: 50,
-	};
+    export let flareSize: number = 120;
+    let xBg = -50, yBg = -50;
 
     function onMouseMove(event: MouseEvent) {
         const eventTarget = event.currentTarget;
@@ -10,24 +8,22 @@
         if (!(eventTarget instanceof Element)) return;
 
         const rect = eventTarget.getBoundingClientRect();
-        const xPercent = ((event.clientX - rect.left) / rect.width) * 100;
-        const yPercent = ((event.clientY - rect.top) / rect.height) * 100;
+        xBg = event.clientX - rect.left - (flareSize / 2);
+        yBg = event.clientY - rect.top - (flareSize / 2);
 
-        styles = {
-            xPercent, yPercent
-        }
+        console.log({ xBg, yBg })
     }
-	
-	$: cssVarStyles = Object.entries(styles)
-		.map(([key, value]) => `--${key}:${value}`)
-		.join(';');
-
 </script>
 
-<section class="bg-primary-200 border-primary-700 border-2 rounded-2xl p-4 w-1/2" on:mousemove={onMouseMove} role="presentation">
+<section style="--x-bg:{xBg}px;--y-bg:{yBg}px;--flare-size:{flareSize}px;" class="border-primary-700 border-2 rounded-2xl p-4 w-1/2 opacity-0 hover:opacity-100 transition-opacity duration-300" on:mousemove={onMouseMove} role="presentation">
     <slot />
 </section>
 
 <style>
-
+    section {
+        background-image: radial-gradient(circle at center, rgba(100, 100, 100, 255), rgba(100, 100, 100, 0) 70%);
+        background-repeat: no-repeat;
+        background-position: var(--x-bg) var(--y-bg);
+        background-size: var(--flare-size) var(--flare-size);
+    }
 </style>
